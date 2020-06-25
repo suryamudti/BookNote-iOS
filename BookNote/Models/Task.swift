3d2190e7f5c9b9a7c94da554609441b3bef1ebf9
suryamudti <suryamudti0128@gmail.com>
2020-06-25 20:38:35 +0700

    add task and tasktore models

---
+++ BookNote/Models/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  BookNote
+//
+//  Created by Suryamudti Salmat on 25/06/20.
+//  Copyright © 2020 Suryamudti Salmat. All rights reserved.
+//
+
+
+struct Task {
+    var name: String
+    var isCompleted = false
+}
---
+++ BookNote/Models/TaskStore.swift
@@ -0,0 +1,11 @@
+//
+//  TaskStore.swift
+//  BookNote
+//
+//  Created by Suryamudti Salmat on 25/06/20.
+//  Copyright © 2020 Suryamudti Salmat. All rights reserved.
+//
+
+class TaskStore {
+    
+}
