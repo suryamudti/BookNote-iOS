82851a83621ce6bc6352a12fe87ad8a7c19677de
suryamudti <suryamudti0128@gmail.com>
2020-06-25 20:24:53 +0700

    Initial Commit

---
+++ BookNote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BookNote
+//
+//  Created by Suryamudti Salmat on 25/06/20.
+//  Copyright © 2020 Suryamudti Salmat. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
